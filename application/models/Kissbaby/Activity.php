e860c7ff5d6f7daec60033d2db92357c4f604d21
hangfang <fangh@me.com>
2018-03-04 00:33:19 +0800

    完成kissbaby全部借口

---
+++ application/models/Kissbaby/Activity.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASE_PATH') OR exit('No direct script access allowed');
+
+class Kissbaby_ActivityModel extends BaseModel {
+    public static $_table = 'activity';
+    public static $_database = 'kissbaby';
+}
---
+++ application/models/Kissbaby/ActivityProduct.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASE_PATH') OR exit('No direct script access allowed');
+
+class Kissbaby_ActivityProductModel extends BaseModel {
+    public static $_table = 'activity_product';
+    public static $_database = 'kissbaby';
+}
