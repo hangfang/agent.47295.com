0e4a75aecdc4b726170b2238b0afcbefdde12d9b
hangfang <fangh@me.com>
2018-03-04 18:41:06 +0800

    提交部分页面

---
+++ template/shop/index/notFound.php
@@ -0,0 +1,14 @@
+<?php 
+defined('BASE_PATH') OR exit('No direct script access allowed');
+include $viewPath.'header.php';
+?>
+<div class="single_top">
+	 <div class="container"> 
+	  <div class="error-404 text-center">
+			<h1>404</h1>
+			<p>页面丢失了</p>
+			<a class="b-home" href="/shop/index/index">回到首页</a>
+		  </div>
+      </div>
+</div>
+<?php include $viewPath.'footer.php';?>
