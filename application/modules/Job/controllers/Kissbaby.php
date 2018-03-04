ea6582ca8782b624640a30c98ac97ec96fabf440
hangfang <fangh@me.com>
2018-03-04 22:42:59 +0800

    提交订单

--- application/modules/Job/controllers/Kissbaby.php
+++ application/modules/Job/controllers/Kissbaby.php
@@ -507,7 +507,7 @@ class KissbabyController extends BasicController{
         $_path = implode('/', $_path);
         
         
-        $_path = preg_replace('/[^0-9a-zA-Z\/\.]/', '', $_path);
+        $_path = preg_replace('/[^0-9a-zA-Z\/\.\\\]/', '', $_path);
         $_path = preg_replace('/\s/', '', $_path);
         $_path = IMAGE_PATH.$_path;
         $_path = str_replace('\\', '/', $_path);
