525d671cc46abb91e05847d447e285cc0c52bd6d
hangfang <fangh@me.com>
2018-03-08 22:50:52 +0800

    完成购物车页面

--- template/shop/activity/index.php
+++ template/shop/activity/index.php
@@ -32,7 +32,7 @@ include $viewPath.'header.php';
     <div class="shop-holder">
          <div class="product-img">
             <a href="/shop/activity/product?activity_id={$_activity['activity_id']}">
-                <img width_bak="225" height_bak="265" data-original="{$_activity['activity_image']}" src="{$STATIC_CDN_URL}{$staticDir}images/default.png" class="lazy img-responsive"  alt="item4">
+                <img width_bak="225" height_bak="265" data-original="{$_activity['activity_image']}" src="{$STATIC_CDN_URL}{$staticDir}images/default225x265.png" class="lazy img-responsive"  alt="item4">
             </a>
             <a href="javascript:void(0);" class="button " style="background:transparent;border:none;"></a>
         </div>
--- template/shop/index/index.php
+++ template/shop/index/index.php
@@ -39,7 +39,7 @@ include $viewPath.'header.php';
     <div class="shop-holder">
          <div class="product-img">
             <a href="/shop/product/detail?product_id={$_product['product_id']}">
-                <img width_bak="225" height_bak="265" data-original="{$_imgSrc}" src="{$STATIC_CDN_URL}{$staticDir}images/default.png" class="lazy img-responsive"  alt="item4">
+                <img width_bak="225" height_bak="265" data-original="{$_imgSrc}" src="{$STATIC_CDN_URL}{$staticDir}images/default225x265.png" class="lazy img-responsive"  alt="item4">
             </a>
             <a href="javascript:void(0);" class="button "></a>
         </div>
