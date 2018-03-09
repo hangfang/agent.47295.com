b8a7f4b1f6567e4fb309a373912bcbc36bd87876
hangfang <fangh@me.com>
2018-03-09 23:52:12 +0800

    处理分页

--- template/shop/category/subcategory.php
+++ template/shop/category/subcategory.php
@@ -32,7 +32,7 @@ include $viewPath.'header.php';
     <div class="shop-holder">
          <div class="product-img">
             <a href="/shop/category/product?category_id={$_category['category_id']}">
-                <img width_bak="225" height_bak="265" data-original="{$_imgSrc}" src="{$STATIC_CDN_URL}{$staticDir}images/default.png" class="lazy img-responsive"  alt="item4">
+                <img width_bak="225" height_bak="265" data-original="{$_imgSrc}" src="{$STATIC_CDN_URL}{$staticDir}images/default215x215.png" class="lazy img-responsive"  alt="item4">
             </a>
             <a href="javascript:void(0);" class="button " style="background:transparent;border:none;"></a>
         </div>
