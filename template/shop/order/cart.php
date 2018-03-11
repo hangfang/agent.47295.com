66b138bca7e0cb869d7db9469013f67515e3171f
hangfang <fangh@me.com>
2018-03-12 00:20:41 +0800

    加入购物车

--- template/shop/order/cart.php
+++ template/shop/order/cart.php
@@ -20,23 +20,14 @@ include BASE_PATH.'/template/common/weui/header.php';
             var cart = JSON.parse(json);
             var html = '';
             for(var i in cart){
-                var product = cart[i];
-                
-                var extra = '';
-                <?php
-                    if(BaseModel::isAdmin()){
-                        echo 'var extra = \'<span class="weui_desc_extra">Vip价:￥\'+ product[\'product_vip_price\'] +\'</span>\';';
-                    }
-                ?>
-
-                            
+                var product = cart[i]; 
                 html += '<div class="weui_media_box weui_media_appmsg">\
         <div class="weui_media_hd">\
             <img class="lazy weui_media_appmsg_thumb" src="'+ product['product_image'].replace('<?php echo CDN_URL_PLACEHOLDER;?>', '<?php echo IMG_CDN_URL;?>') +'" onerror="this.src=\'<?php echo STATIC_CDN_URL.$staticDir;?>images/qrcode_for_gh_a103c9f558fa_258.jpg\'" >\
         </div>\
         <div class="weui_media_bd">\
             <h4 class="weui_media_title" style="margin: 0px;">'+ product['product_name'] +'</h4>\
-            <p class="weui_media_desc" style="height: 40px;width: 100%;margin: 0;position:relative;"><span class="weui_btn weui_btn_mini weui_btn_primary" style="line-height: 33px;position: absolute;bottom: 0;width: 59px;" onclick="location.href=\'/shop/product/detail?product_id='+ product['product_id'] +'\'">详情</span>'+ extra +'<span class="cart_plus" data=\''+ JSON.stringify(product) +'\'></span><span class="cart_number">'+ product['product_number'] +'</span><span class="cart_minus" data=\''+ JSON.stringify(product) +'\'></span></p>\
+            <p class="weui_media_desc" style="height: 40px;width: 100%;margin: 0;position:relative;"><span class="weui_btn weui_btn_mini weui_btn_primary" style="line-height: 33px;position: absolute;bottom: 0;width: 59px;" onclick="location.href=\'/shop/product/detail?product_id='+ product['product_id'] +'\'">详情</span><span class="cart_plus" data=\''+ JSON.stringify(product) +'\'></span><span class="cart_number">'+ product['product_number'] +'</span><span class="cart_minus" data=\''+ JSON.stringify(product) +'\'></span></p>\
         </div>\
     </div>';
 
