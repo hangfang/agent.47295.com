4f8884cbe2dfee15e8804bef1ca0d80f02b48f6d
fanghang <fanghang@fujiacaifu.com>
2018-03-21 14:58:08 +0800

    添加物流页面

--- template/shop/bill/express.php
+++ template/shop/bill/express.php
@@ -6,6 +6,7 @@ include BASE_PATH.'/template/common/weui/header.php';
     <div class="weui_panel_hd">物流信息</div>
     <div class="weui_panel_bd">
         <?php
+        if($expressDetail){
             foreach($expressDetail as $_detail){
                 echo <<<EOF
 <div class="weui_media_box weui_media_text">
@@ -16,6 +17,17 @@ include BASE_PATH.'/template/common/weui/header.php';
 </div>
 EOF;
             }
+        }else{
+            $time = date('Y-m-d H:i:s');
+            echo <<<EOF
+<div class="weui_media_box weui_media_text">
+    <p class="weui_media_desc">暂无物流信息</p>
+    <ul class="weui_media_info">
+        <li class="weui_media_info_meta">{$time}</li>
+    </ul>
+</div>
+EOF;
+        }
         ?>
     </div>
 </div>
