bfffeea30b20ff68496885f036f1fa8c092c4773
hangfang <fangh@me.com>
2018-03-25 10:48:52 +0800

    搜索的bug

--- template/shop/product/search.php
+++ template/shop/product/search.php
@@ -315,3 +315,12 @@ EOF;
     });
 </script>
 <?php include BASE_PATH.'/template/common/weui/footer.php';?>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?eb932971a3374ca003a8cbf8424d01ea";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
